21436646613776051fb4effa4fd0d7e1e75717ae
emailgregn <emailgregn@gmail.com>
2016-10-15 15:07:59 +0200

    forgot hashbang
    Former-commit-id: 4d202fdd40f1c382ae69d073d6c38866b0fe52d5

--- src2/everything.sh
+++ src2/everything.sh
@@ -1,4 +1,4 @@
- /bin/sh
+ #!/bin/sh
 ./ttc.py preprocess modeldata.h5 ../data/population_v2.1/*.log
 ./ttc.py train --reset model.ttc modeldata.h5
 ./ttc.py predict model.ttc ../data/population_v2.1/2007-11-11.log
