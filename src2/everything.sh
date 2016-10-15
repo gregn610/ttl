6c6609d4907e407e97bd0cf3208051383972cafd
emailgregn <emailgregn@gmail.com>
2016-10-15 14:58:02 +0200

    everything.sh wrapper
    Former-commit-id: 7353b49a06b0edd522f4d4c1a0102169c5792ac8

---
+++ src2/everything.sh
@@ -0,0 +1,5 @@
+ /bin/sh
+./ttc.py preprocess modeldata.h5 ../data/population_v2.1/*.log
+./ttc.py train --reset model.ttc modeldata.h5
+./ttc.py predict model.ttc ../data/population_v2.1/2007-11-11.log
+
